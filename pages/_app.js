a6435b93ced9b9da380d21b2eb0d499518c1ff6d
Justin Moon <mail@justinmoon.com>
2020-12-14 19:20:20 -0600

    Add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
+import { useEffect } from "react"
 import { ThemeProvider, CSSReset } from "@chakra-ui/core"
+import Router from "next/router"
+import * as Fathom from "fathom-client"
+
 import { AuthProvider } from "utils/auth"
 import theme from "styles/theme"
 
+Router.events.on("routeChangeComplete", () => {
+  Fathom.trackPageview()
+})
+
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      Fathom.load("SIJYMLCS", {
+        includedDomains: ["talkingpoints.vercel.app"],
+      })
+    }
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
